058e6ea452b8721a41fd3d424706847b52714151
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-05 19:07:18 +0530

    chore: added db configs for type orm to establish conn with db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/users/user.module';
+import { HomeModule } from './modules/homes/home.module';
+import { UserHomeMappingModule } from './modules/user_home_mappings/user_home_mapping.module';
+import { User } from './modules/users/user.entity';
+import { Home } from './modules/homes/home.entity';
+import { UserHomeMapping } from './modules/user_home_mappings/user_home_mapping.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1', 
+      username: 'root',
+      password: '6equj5_root',
+      database: 'home_db',
+      entities: [User, Home, UserHomeMapping],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([User, Home, UserHomeMapping]),
+    UserModule,
+    HomeModule,
+    UserHomeMappingModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
