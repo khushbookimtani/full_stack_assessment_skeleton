a434b413a4713320de1c28633f36bbc27ff16066
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-05 19:12:17 +0530

    chore: made changes in sql script to add constraints in user table and id in the mapping

--- sql/99_final_db_dump.sql
+++ sql/99_final_db_dump.sql
@@ -1,7 +1,7 @@
 -- Create the 'user' table with UUID as the primary key
 CREATE TABLE `user` (
   `id` CHAR(36) PRIMARY KEY,
-  `username` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100) NOT NULL UNIQUE,
   `email` VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -19,14 +19,14 @@ CREATE TABLE `home` (
 
 -- Create the 'user_home_mapping' table to establish a many-to-many relationship
 CREATE TABLE `user_home_mapping` (
+  `id` CHAR(36) PRIMARY KEY,
   `user_id` CHAR(36),
   `home_id` CHAR(36),
-  PRIMARY KEY (`user_id`, `home_id`),
   FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
   FOREIGN KEY (`home_id`) REFERENCES `home`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Populate the 'user' table from 'user-home' table
+-- Populate the 'user' table from 'user_home' table
 INSERT INTO `user` (`id`, `username`, `email`)
 SELECT UUID(), `username`, `email`
 FROM (SELECT DISTINCT `username`, `email` FROM `user_home`) as distinct_users;
@@ -36,17 +36,16 @@ INSERT INTO `home` (`id`, `street_address`, `state`, `zip`, `sqft`, `beds`, `bat
 SELECT UUID(), `street_address`, `state`, `zip`, `sqft`, `beds`, `baths`, `list_price`
 FROM `user_home`;
 
--- Populate the 'user_home_mapping' table from 'user-home'
-INSERT INTO `user_home_mapping` (`user_id`, `home_id`)
+-- Populate the 'user_home_mapping' table from 'user_home'
+INSERT INTO user_home_mapping (id, user_id, home_id)
 SELECT 
-  u.`id` AS `user_id`, 
-  h.`id` AS `home_id`
+  UUID() AS id, 
+  u.id AS user_id, 
+  h.id AS home_id
 FROM 
-  `user_home` uh
+  user_home uh
 JOIN 
-  `user` u ON uh.`username` = u.`username` AND uh.`email` = u.`email`
+  user u ON uh.username = u.username AND uh.email = u.email
 JOIN 
-  `home` h ON uh.`street_address` = h.`street_address` AND uh.`state` = h.`state` AND uh.`zip` = h.`zip`;
+  home h ON uh.street_address = h.street_address AND uh.state = h.state AND uh.zip = h.zip;
 
--- Drop the old 'user_home' table
-DROP TABLE `user_home`;
