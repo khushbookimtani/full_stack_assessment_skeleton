7e267f165112a445045c2d71f650759e52f41b34
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-08 14:59:58 +0530

    refactor: added cor policy for apis

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { useContainer } from 'class-validator';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-
-  await app.listen(3000);
+  app.enableCors(); 
+  await app.listen(3030);
 }
 bootstrap();
