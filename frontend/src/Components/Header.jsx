0957881accfdc9e73ebdc8ae1e010d37b75eb41b
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-08 15:09:03 +0530

    refactor: fetching data, and managing re-renders on state changes

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react'
-import { Dropdown } from './Dropdown'
-import { Button } from './Button'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUsers } from '../store/userSlice'
+import { useGetAllUsersQuery } from '../services/userApi'
+import { SelectUser } from './SelectUser'
 
 export const Header = () => {
-    const [showDropdown, setShowDropdown] = useState(false)
+    
+    const { data, error, isLoading } = useGetAllUsersQuery('') 
+    
+    const users = useSelector(state => state.users);
+    const dispatch = useDispatch();
 
-    const toggleDropdownShow = () => {
-        setShowDropdown(!showDropdown)
-    }
+    useEffect(()=>{
+        dispatch(setUsers({data}));
+        console.log(users)
+    }, [data]);
+
      return (
-    <nav class="bg-white border-gray-200 flex justify-center">
-        <div>
-        <Button type="button" buttonStyle="primary" onClick={toggleDropdownShow}>Select Users <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-</svg>  </Button>
-        <Dropdown showDropdown={showDropdown}/>
-        </div>
-    </nav>
+        <nav className="relative bg-white border-gray-200">
+            <SelectUser users={users.value.data}/>
+        </nav>
   )
 }
