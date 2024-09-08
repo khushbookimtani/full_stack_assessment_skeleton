bbe9ea35624aeff019051f5a35c0216f612b2dd8
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-08 15:10:58 +0530

    refactor: orchestrating re-renders after state changes

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from '../Components/Card'
-import { Dropdown } from '../Components/Dropdown'
 import { Header } from '../Components/Header'
+import { Modal } from '../Components/Modal'
+import { Pagination } from '../Components/Pagination'
+import { useDispatch, useSelector } from 'react-redux'
+import { CardList } from '../Components/CardList'
+import { useGetAllHomesByUserQuery } from '../services/homeApi'
+import { setHomes } from '../store/homeSlice'
 
-const data = { "homeId": "home1", "address": "123 Elm Street", "city": "Springfield", "state": "IL", "zipCode": "62701", "price": "$250,000" }
 
 export const Home = () => {
-    return (
+  const [showModal, setShowModal] = useState(false)
+  const [selectedHome, setSelectedHome] = useState('')
+  
+  const toggleModalShow = (homeId) => {
+    setShowModal(!showModal)
+    setSelectedHome(homeId)
+  }
+
+  const [didUpdate, setDidUpdate] = useState(false)
+  const dispatch = useDispatch();
+    const selectedUser = useSelector(state => state.users.selectedUser);
+    const nextPage = useSelector(state => state.homes.nextPage)
+    const homes = useSelector(state => state.homes)
+    
+    const { data,refetch } = useGetAllHomesByUserQuery({userId:selectedUser,page:nextPage})
+
+    useEffect(() => {
+     console.log("Home rendered")
+        if(data){
+          dispatch(setHomes(data));
+          console.log("Homes", homes.data);
+        }
+    },[data])
+
+
+  return (
     <>
-        <Header/>
-        <Card data = {data}/>
+      <Header />
+      { selectedUser && <CardList homes={homes.data} toggleModalShow={toggleModalShow} didUpdate={didUpdate}/>}
+
+      { selectedHome && <Modal showModal={showModal} setShowModal={setShowModal} selectedHome={selectedHome} setSelectedHome={setSelectedHome} refetch={refetch}/>
+      }
+      {
+        selectedUser && <Pagination />
+      }
     </>
   )
 }
