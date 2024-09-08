0703bf79d934a7c240fddb25ed62b2b6381a5b2b
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-08 15:01:54 +0530

    refactor:  added additional props to make button component reusable accross app

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -4,10 +4,13 @@ export const Button = ({
     type='button',
     buttonStyle = 'primary',
     children,
-    onClick
+    onClick,
+    attributes=null,
+    classes=''
 }) => {
   return (
-    <button className={`inline-flex items-center px-3 py-2 mx-1 text-sm font-medium text-center rounded-lg  text-white ${buttonStyle == 'primary' ? 'bg-blue-500' : 'bg-gray-400'}`} type={type} onClick={() => onClick()}>
+    <button {...attributes} className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg  text-white ${buttonStyle == 'primary' ? 'bg-blue-500' : 'bg-gray-400'} ${classes}`} type={type} onClick={() => onClick()}
+    >
         { children }
     </button>
   )
