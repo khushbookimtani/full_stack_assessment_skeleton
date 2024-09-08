04515ba8a1ccdfb1bc80c4d9780eb7a98adc9c36
Khushboo Kimtani <khushboo@onestopengineering.in>
2024-09-08 16:39:03 +0530

    refactor: changes anchor to div

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = ({data, toggleModalShow}) => {
     return (
     <>
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
-        <a href="#">
+        <div >
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{data.streetAddress || <Skeleton />}</h5>
             <ul className="text-sm font-medium text-gray-900 py-2">
                 <li className="w-full py-1">List Price: {data.listPrice} </li>
@@ -17,7 +17,7 @@ export const Card = ({data, toggleModalShow}) => {
                 <li className="w-full py-1">Beds: {data.beds} </li>
                 <li className="w-full py-1">Baths:  {data.baths}</li>                
             </ul>   
-        </a>
+        </div>
         <Button type='submit' buttonStyle='primary' onClick={() => toggleModalShow(data.id)}>Edit User</Button>
     </div>
     </>
